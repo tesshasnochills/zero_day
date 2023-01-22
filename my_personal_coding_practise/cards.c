2820c21bf78e20078f950331014ff94b2e02a4f5
Tess Ng'ang'a <tess.nganga2020@gmail.com>
2023-01-22 19:43:11 +0300

    Card counting program using switches and loops

--- my_personal_coding_practise/cards.c
+++ my_personal_coding_practise/cards.c
@@ -11,31 +11,44 @@
 int main(void)
 {
 	char card_name[3];
+	int count = 0;
 
+	while (card_name[0] != 'X')
+	{
 	puts("Enter the card_name: ");
 	scanf("%2s", card_name);
 	int val = 0;
 
-	if (card_name[0] == 'K')
+	switch (card_name[0])
 	{
-		val = 10;
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				break;
+			case 'A':
+				val = 11;
+				break;
+			case 'X':
+					continue;
+			default:
+				val = atoi(card_name);
+				if ((val < 1) || (val > 10))
+			{
+				puts("I dont understand that value!");
+				continue;
+			}
 	}
-	else if (card_name[0] == 'Q')
-	{
-		val = 10;
+		if ((val > 2) && (val < 7))
+		{
+		count++;
+		}
+		else if (val == 10)
+		{
+			count--;
+		}
+
+		printf("Current count: %i\n", count);
 	}
-	else if (card_name[0] == 'J')
-	{
-		val = 10;
-	}
-	else if (card_name[0] == 'A')
-	{
-		val = 11;
-	}
-	else
-	{
-	val = atoi(card_name);
-	}
-	printf("The card value is; %i\n", val);
 	return (0);
 }
